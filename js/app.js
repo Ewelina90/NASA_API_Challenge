c582fde296606d99923f9bf530aa9d3592c9a07d
Ewelina Dziura <ewedziura@gmail.com>
2017-06-17 22:09:22 +0200

    ajax gallery and slajder

---
+++ js/app.js
@@ -0,0 +1,65 @@
+$(() => {
+
+    const $welcome = $('.welcome');
+    const $loadBtn = $('.load-btn');
+    const $gallery = $('.container');
+
+    //randomize number form range
+    const getRandomInt = (min,max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    //randomize date
+    const getRandomDate = () => {
+        const day = getRandomInt(1,28);
+        const month = getRandomInt(1,12);
+        const year = getRandomInt(2010,2016);
+
+        return year+'-'+month+'-'+day;
+    }
+    // create new pictures for gallery
+    const addNewImages = (link) => {
+        const $newDiv = $('<div>',{class:"group-images"});
+        const $newUl = $('<ul>',{class:"images"});
+
+        for(let i = 0; i < link.length; i++){
+            let $newLi = $('<li>');
+            let $newImg = $('<img>',{src: link[i] ,alt:'Photo of a Mars taken by the Curiosity rover'});
+            $newLi.append($newImg);
+            $newUl.append($newLi);
+        }
+        $newDiv.append($newUl);
+        $gallery.append($newDiv);
+    }
+
+    const getRandomSol = () => {
+        const sol = getRandomInt(1,1728);
+        return sol;
+    }
+    //Btn - load more pictures into gallery
+    $loadBtn.on('click',function(){
+        $.ajax({
+            url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=8OMH6j4AYg49k56NSqvfwKHgwxOgb2XiR2KEVSJ7&sol='+getRandomSol()
+        }).done(response => {
+            const links = [];
+            for(let i = 0; i < 6 ;i++) {
+                links.push(response.photos[i].img_src);
+            }
+            addNewImages(links);
+        }).fail(error => {
+            console.log('wystąpił error');
+        })
+
+    });
+    
+    //Image for slaider
+    $.ajax({
+        url: 'https://api.nasa.gov/planetary/apod?&api_key=8OMH6j4AYg49k56NSqvfwKHgwxOgb2XiR2KEVSJ7&date='+getRandomDate()
+    }).done(response => {
+        const img = 'url("'+response.url+'")';
+        console.log(img);
+        $welcome.css('background-image',img);
+    }).fail(error => {
+        console.log('error');
+    })
+});
